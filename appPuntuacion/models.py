ae5653bb29a2028de45fbcaa9544c61e3b0b7212
LuisVI <lumavi.lv@gmail.com>
2021-05-26 19:39:56 -0500

    JSON Parse not working

--- appPuntuacion/models.py
+++ appPuntuacion/models.py
@@ -5,3 +5,5 @@ class Localidad(models.Model):
 
     nombre=models.CharField(max_length=30)
     puntuacion=models.IntegerField()
+    def __str__(self):
+        return self.nombre
--- appPuntuacion/views.py
+++ appPuntuacion/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import Localidad
+from json import dumps
 
 
 # Create your views here.
 
 def puebla(request):
-    return render(request, 'appPuntuacion/puebla.html',)
+    Localidades = Localidad.objects.all()
+    nombre= Localidades.nombre
+    puntaje = Localidades.puntaje
+    return render(request, 'appPuntuacion/puebla.html', {'Nombre': dumps(nombre), 'Puntaje':dumps(puntaje)})
