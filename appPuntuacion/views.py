42b266e6fc6c3b090609e5f69741be0742c92b64
LuisVI <lumavi.lv@gmail.com>
2021-05-28 16:44:44 -0500

    Rango Dinamico Estrellas

--- appPuntuacion/views.py
+++ appPuntuacion/views.py
@@ -1,21 +1,25 @@
 from django.shortcuts import render
 from .models import Localidad
+from django.db.models import Max
 from json import dumps
 
 
 # Create your views here.
 def localidades(request):
-    Localidades=Localidad.objects.order_by('-puntuacion')
+    Localidades=Localidad.objects.order_by('nombre')
     nombre=[]
     puntuacion=[]
     for localidad in Localidades:
         nombre.append(localidad.nombre)
         puntuacion.append(localidad.puntuacion)
-    return render(request, 'appPuntuacion/localidades.html', {'Nombre':dumps(nombre), 'Puntuacion':dumps(puntuacion)})
+    maxPuntuacion=Localidad.objects.all().aggregate(Max('puntuacion'))
+    return render(request, 'appPuntuacion/localidades.html', {'Nombre':dumps(nombre), 'Puntuacion':dumps(puntuacion), 'MaxPuntuacion':maxPuntuacion["puntuacion__max"]})
 
 def localidadSelect(request):
+    maxPuntuacion=Localidad.objects.all().aggregate(Max('puntuacion'))
     LocalidadSelect=request.GET.get('localidad')
     local=Localidad.objects.get(nombre__icontains=LocalidadSelect)
     nombreLocalidad= local.nombre
     puntuacionLocalidad=local.puntuacion
-    return render(request, 'appPuntuacion/localidadSelect.html', {'NombreLocalidad':nombreLocalidad, 'PuntuacionLocalidad':dumps(puntuacionLocalidad)})
+    return render(request, 'appPuntuacion/localidadSelect.html', {'NombreLocalidad':nombreLocalidad, 'PuntuacionLocalidad':dumps(puntuacionLocalidad), 'MaxPuntuacion':dumps(maxPuntuacion["puntuacion__max"])})
+
