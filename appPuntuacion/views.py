ee99d5ceaf4aac8da2b76f5c90b9fd2eaf55487d
balam981 <vargasvidalbalammail.com>
2021-05-29 18:06:44 -0500

    final

--- appPuntuacion/views.py
+++ appPuntuacion/views.py
@@ -16,10 +16,16 @@ def localidades(request):
     return render(request, 'appPuntuacion/localidades.html', {'Nombre':dumps(nombre), 'Puntuacion':dumps(puntuacion), 'MaxPuntuacion':maxPuntuacion["puntuacion__max"]})
 
 def localidadSelect(request):
+    Localidades=Localidad.objects.order_by('nombre')
+    nombre=[]
+    puntuacion=[]
+    for localidad in Localidades:
+        nombre.append(localidad.nombre)
+        puntuacion.append(localidad.puntuacion)
     maxPuntuacion=Localidad.objects.all().aggregate(Max('puntuacion'))
     LocalidadSelect=request.GET.get('localidad')
     local=Localidad.objects.get(nombre__icontains=LocalidadSelect)
     nombreLocalidad= local.nombre
     puntuacionLocalidad=local.puntuacion
-    return render(request, 'appPuntuacion/localidadSelect.html', {'NombreLocalidad':nombreLocalidad, 'PuntuacionLocalidad':dumps(puntuacionLocalidad), 'MaxPuntuacion':dumps(maxPuntuacion["puntuacion__max"])})
+    return render(request, 'appPuntuacion/localidadSelect.html', {'Nombre':dumps(nombre), 'Puntuacion':dumps(puntuacion),'NombreLocalidad':nombreLocalidad, 'PuntuacionLocalidad':dumps(puntuacionLocalidad), 'MaxPuntuacion':dumps(maxPuntuacion["puntuacion__max"])})
 
